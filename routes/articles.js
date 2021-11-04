8e67a52d2d9fc630a7fc22f30582496fa207fd0d
Bertyn Boulikou <bboulikou@gmail.com>
2021-11-04 16:53:52 +0100

    edit path view

--- routes/articles.js
+++ routes/articles.js
@@ -3,18 +3,18 @@ const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-  res.render('articles/new', { article: new Article() })
+  res.render('new', { article: new Article() })
 })
 
 router.get('/edit/:id', async (req, res) => {
   const article = await Article.findById(req.params.id)
-  res.render('articles/edit', { article: article })
+  res.render('edit', { article: article })
 })
 
 router.get('/:slug', async (req, res) => {
   const article = await Article.findOne({ slug: req.params.slug })
   if (article == null) res.redirect('/')
-  res.render('articles/show', { article: article })
+  res.render('show', { article: article })
 })
 
 router.post('/', async (req, res, next) => {
@@ -42,7 +42,7 @@ function saveArticleAndRedirect(path) {
       article = await article.save()
       res.redirect(`/articles/${article.slug}`)
     } catch (e) {
-      res.render(`articles/${path}`, { article: article })
+      res.render(`${path}`, { article: article })
     }
   }
 }
