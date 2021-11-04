0ce7dd5e51f105e39d241271789c8d4268d9ae8e
Bertyn Boulikou <bboulikou@gmail.com>
2021-11-04 15:36:07 +0100

    ajout engine ejs

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express();
 const PORT =3000
-/* const apiRouter = require("./apiRouter").router; */
+const articleRouter = require("./routes/articles"); 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.set('view engine','ejs')
+app.use(articleRoute)
 app.listen(PORT, () => {
     console.log(`Application listening on port ${PORT}!`);
 });
